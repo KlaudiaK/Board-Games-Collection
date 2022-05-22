9e03fda3a8df4ab8af7347d419482c9bd330eb37
MichalxPZ <michauz2001@gmail.com>
2022-05-22 13:52:23 +0200

    buf fix

--- app/src/main/java/com/klaudiak/gamescollector/di/AppModule.kt
+++ app/src/main/java/com/klaudiak/gamescollector/di/AppModule.kt
@@ -11,6 +11,8 @@ import com.klaudiak.gamescollector.data.local.GameDao
 import com.klaudiak.gamescollector.data.remote.NetworkService
 import com.klaudiak.gamescollector.data.remote.reponses.GameResponse
 import com.github.slashrootv200.retrofithtmlconverter.HtmlConverterFactory
+import com.klaudiak.gamescollector.data.local.ExtensionDao
+import com.klaudiak.gamescollector.data.local.InfoDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -21,63 +23,25 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class AppModule {
-
+object AppModule {
 
     @Provides
     @Singleton
-
     fun provideNetworkMapper() : EntityMapper<GameResponse.Item, Game> {
         return NetworkMapper()
     }
 
     @Provides
+    fun provideGameDao(db: AppDatabase): GameDao  = db.gameDao()
+    @Provides
+    fun provideExtensionDao(db: AppDatabase): ExtensionDao = db.extensionDao()
+    @Provides
+    fun provideInfoDao(db: AppDatabase): InfoDao = db.infoDao()
+
+
+    @Provides
     @Singleton
-    fun provideDatabase(@ApplicationContext appContext: Context) = AppDatabase.getDatabase(appContext)
-
-
-    @Provides
-    fun provideGameDao(db: AppDatabase) = db.gameDao()
-    @Provides
-    fun provideExtensionDao(db: AppDatabase) = db.extensionDao()
-    @Provides
-    fun provideInfoDao(db: AppDatabase) = db.infoDao()
-
-
-
-}
-
-
-@Module
-@InstallIn(SingletonComponent::class)
-object RepositoryModule {
-
-    @Provides
-    fun provideMainRepository(
-        gameDao: GameDao,
-        networkService: NetworkService,
-        databaseMapper: DatabaseMapper,
-        networkMapper: NetworkMapper
-    ) : MainRepository {
-        return MainRepository(gameDao, networkService, databaseMapper, networkMapper)
-    }
-}
-
-
-
-@Module
-@InstallIn(SingletonComponent::class)
-object NetworkModule {
-
-
-    @Singleton
-    @Provides
-    fun provideRetrofitService(): NetworkService = Retrofit.Builder()
-        .baseUrl("https://boardgamegeek.com/wiki/page/BGG_XML_API2")
-        .addConverterFactory(HtmlConverterFactory.create("https://boardgamegeek.com/wiki/page/BGG_XML_API2"))
-        .build()
-        .create(NetworkService::class.java)
-
+    fun provideDatabase(@ApplicationContext appContext: Context) : AppDatabase = AppDatabase.getDatabase(appContext)
 
 }
 
--- app/src/main/java/com/klaudiak/gamescollector/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/klaudiak/gamescollector/viewmodel/MainViewModel.kt
@@ -10,11 +10,13 @@ import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+
+
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val mainRepository: MainRepository,
     //private val savedStateHandle: SavedStateHandle
-) : ViewModel(), LifecycleObserver  {
+) : ViewModel()  {
 
     private val _dataState: MutableLiveData<DataState<List<Game>>> = MutableLiveData()
     val dataState: LiveData<DataState<List<Game>>>
